fe0127bd991b65c0d9e326536b633de932ad488e
vietbuiminh <buiminhviet.2002@gmail.com>
2020-04-26 20:35:27 -0700

    Added save on local disk

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 console.log('EW')
-const listsContainer = document.querySelector('[data-lists]')
 
-let lists = ['name', 'todo']
+const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+
+
+newListForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const listName = newListInput.value
+  if (listName == null || listName === '') return
+  const list = createList(listName)
+  newListInput.value = null
+  lists.push(list)
+  saveAndRender()
+})
+
+function createList(name) {
+  return {id: Date.now().toString(), name: name, task: []}
+}
+
+function saveAndRender() {
+  save()
+  render()
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+}
 
 function render() {
   clearElement(listsContainer)
   lists.forEach(list => {
     const listElement = document.createElement('li')
+    listElement.dataset.listId = list.id
     listElement.classList.add("list-name")
-    listElement.innerText = list
+    listElement.innerText = list.name
     listsContainer.appendChild(listElement)
   })
 }
